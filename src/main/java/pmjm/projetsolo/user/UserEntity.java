41d780a269c78202aa75e2327d5479e50c351ca1
Mathieu Coulibaly <mathieu.coulibaly@reseau.sncf.fr>
2023-11-06 22:13:38 +0100

    get and post ok, put not working on postman

--- src/main/java/pmjm/projetsolo/user/UserEntity.java
+++ src/main/java/pmjm/projetsolo/user/UserEntity.java
@@ -1,25 +1,20 @@
-package user;
+package pmjm.projetsolo.user;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.web.bind.annotation.RestController;
-
-@NoArgsConstructor
-@RestController
+import lombok.*;
+@RequiredArgsConstructor
 @Setter
 @Getter
 @Entity
-public class user {
+public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    public long id;
+    public Long id;
 
     public String username;
     public String email;
---
+++ src/main/java/pmjm/projetsolo/user/UserService.java
@@ -0,0 +1,36 @@
+package pmjm.projetsolo.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+    public UserService(UserRepository userRepository){
+    this.repository = userRepository;
+    }
+    public List<UserEntity> getAll(){
+        return repository.findAll();
+    }
+
+    public UserEntity getById(Long id) {
+        return repository.findById(id)
+                .orElseThrow();
+    }
+
+    public UserEntity createUser(UserEntity user){
+        return repository.save(user);
+    }
+
+
+    public UserEntity updateUser(UserEntity user, Long id) {
+        UserEntity existingUser =  repository.findById(id)
+                .orElseThrow();
+        existingUser.setUsername(user.getUsername());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPassword(user.getPassword());
+        return  repository.save(user);
+    }
+}
